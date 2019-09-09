3145a4113363c5ecf01fc120ab3747c00aef1558
Ogethel <10718916@my.uvu.edu>
2019-09-09 14:23:29 -0600

    Trying to Follow Anthony

---
+++ HyperCasual_Unity3.5f1/Assets/Scripts/GameActionHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandler : MonoBehaviour
+{
+    public GameAction GameActionObj;
+    public UnityEvent handlerEvent;
+    private void Start ()
+    {
+        GameActionObj.action += Action;
+    }
+    private void Action()
+    {
+        handlerEvent.Invoke();
+    }
+
+    
+}
