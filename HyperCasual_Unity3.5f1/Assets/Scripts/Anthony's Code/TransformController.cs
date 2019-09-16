35fcb1ee66a50576f533be88d807372537760194
Ogethel <10718916@my.uvu.edu>
2019-09-16 16:48:19 -0600

    Working on Making a State Machine with Actions

---
+++ HyperCasual_Unity3.5f1/Assets/Scripts/Anthony's Code/TransformController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TransformController : MonoBehaviour
+{
+   public FloatData speed;
+
+   private void Update()
+   {
+      var speedVector = new Vector3(x:speed.value, y: 0, z: 0) * Time.deltaTime;
+      transform.Translate(speedVector);
+   }
+}
