bafce42aff16ffadf26b1d35dba7906a8478e33c
Ogethel <10718916@my.uvu.edu>
2019-09-18 16:37:51 -0600

    Adding my Game Scene

---
+++ HyperCasual_Unity3.5f1/Assets/Scripts/Anthony's Code/ActionCalls.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActionCalls : MonoBehaviour
+{
+    public float speed = 1f;
+    public float rotateSpeed = 10f;
+    public float scaleSpeed = .01f;
+
+    private Vector3 location, rotations, scales;
+    private UnityAction transformAction;
+
+    private void Start()
+    {
+        transformAction = OnMove;
+    }
+    public enum Transformstates
+    {
+        Move,
+        Rotate,
+        Scale
+
+    }
+
+    private void OnMouseDown()
+    {
+       //if (transformAction == OnMove)
+           
+    }
+
+   
+
+    void Update() //Update should be used to call functions not define them
+    {
+        transformAction();
+    }
+
+    public void OnMove()
+    {
+        location.x = speed * Time.deltaTime; //Normalize values base on frame rate
+        transform.Translate(location);
+    }
+
+    public void OnRotate()
+    {
+        rotations.y = rotateSpeed*Time.deltaTime;
+        transform.Rotate(rotations);
+    }
+
+    public void OnScale()
+    {
+        scales.Set(newX: scaleSpeed, newY: scaleSpeed, newZ: scaleSpeed);
+        transform.localScale = scales;
+    }
+}
