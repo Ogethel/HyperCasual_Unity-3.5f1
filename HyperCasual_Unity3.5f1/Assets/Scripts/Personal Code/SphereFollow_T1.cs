7d66796aa34aa2be06963281da867db4783e272c
McKade <10718916@my.uvu.edu>
2019-09-21 20:43:02 -0600

    Baking NavMesh + Work with Enemy Script

---
+++ HyperCasual_Unity3.5f1/Assets/Scripts/Personal Code/SphereFollow_T1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SphereFollow_T1 : MonoBehaviour
+{
+    public GameObject target = null;
+
+    private NavMeshAgent nma = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        nma = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nma.SetDestination(target.transform.position);
+    }
+}
