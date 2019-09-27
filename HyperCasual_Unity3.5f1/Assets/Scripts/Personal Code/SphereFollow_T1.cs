2dad94732274254cfa07596d52c454c6e3b3b99f
McKade <10718916@my.uvu.edu>
2019-09-26 23:01:40 -0600

    Scripting

--- HyperCasual_Unity3.5f1/Assets/Scripts/Personal Code/SphereFollow_T1.cs
+++ HyperCasual_Unity3.5f1/Assets/Scripts/Personal Code/SphereFollow_T1.cs
@@ -8,16 +8,26 @@ public class SphereFollow_T1 : MonoBehaviour
     public GameObject target = null;
 
     private NavMeshAgent nma = null;
+    public float speed = 10f;
     
     // Start is called before the first frame update
     void Start()
     {
         nma = this.GetComponent<NavMeshAgent>();
+        nma.speed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
         nma.SetDestination(target.transform.position);
+
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "PlayerTower")
+        {
+            Debug.Log("Tag");
+        }
     }
 }
