fc35264dd35f495af1038f28c06ab9068413a000
Ogethel <10718916@my.uvu.edu>
2019-09-30 16:49:19 -0600

    Adding NavMesh with Anthony

---
+++ HyperCasual_Unity3.5f1/Assets/Scripts/Anthony's Code/AiWithNavMesh.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class AiWithNavMesh : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    public Transform player;
+
+    public Transform destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        destination = transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        destination = player;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        destination = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = destination.position;
+    }
+}
