076c03aaf93eda94d30af0fe45ca1140dfdc384e
Ogethel <10718916@my.uvu.edu>
2019-10-30 16:46:07 -0600

    Adding Anthony's Character Controller

---
+++ HyperCasual_Unity3.5f1/Assets/Scripts/Anthony's Code/CharacterBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterBase : MonoBehaviour
+{
+   public CharacterController Controller { get; set; }
+
+   protected Vector3 Location;
+   protected Vector3 Orientation;
+   public float speed = 10f;
+   public float orientSpeed = 3f;
+
+   public abstract void Move();
+
+}
---
+++ HyperCasual_Unity3.5f1/Assets/Scripts/Anthony's Code/CharacterPrime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterPrime : CharacterBase
+{
+  public override void Move()
+  {
+    Location.Set(newX:Input.GetAxis("Vertical")*speed, newY:0, newZ:0);
+    Orientation.y = Input.GetAxis("Horizontal") * orientSpeed;
+
+    Transform transform;
+    (transform = Controller.transform).Rotate(Orientation);
+    Location = Controller.transform.TransformDirection(transform.position);
+
+    Controller.Move(Location * Time.deltaTime);
+  }
+}
