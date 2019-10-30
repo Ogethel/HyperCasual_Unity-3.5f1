4ed1afe7a9dd2636ac57fde88b5cce04862c46f0
McKade <10718916@my.uvu.edu>
2019-10-29 22:33:44 -0600

    Adding the ability to add turrets in Game

---
+++ HyperCasual_Unity3.5f1/Assets/Node.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+       if (turret != null)
+        {
+            Debug.Log("Can't build there! = TODO: Display on Screen.");
+            return;
+        }
+        //Build a Turret
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+    private void OnMouseEnter()//Everytime the mouse moves over the object
+    {
+        rend.material.color=hoverColor;
+        
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
