2f85e598349ae680df4ddc0d8de218cd081f7117
Ogethel <10718916@my.uvu.edu>
2019-11-13 17:35:17 -0700

    Adding Coin Collections

---
+++ HyperCasual_Unity3.5f1/Assets/Scripts/Anthony's Code/Coin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class Coin : ScriptableObject
+{
+   public UnityAction<int> coinAction;
+   
+   public int value = 10;
+
+   public void Spend()
+   {
+      coinAction?.Invoke(value);
+   }
+}
