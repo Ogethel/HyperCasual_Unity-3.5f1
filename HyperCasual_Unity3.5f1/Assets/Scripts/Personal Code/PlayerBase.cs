a557661f70f6b2c6eda9f9b3d205ff80fd37915e
McKade <10718916@my.uvu.edu>
2019-11-22 15:04:10 -0700

    Trying to add Highlight to Player Base on Mouse Hover

---
+++ HyperCasual_Unity3.5f1/Assets/Scripts/Personal Code/PlayerBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBase : MonoBehaviour
+{
+    public Color hoverColor;
+
+    public GameObject playerTower;
+
+    private Renderer rend;
+    private Color startColor;
+
+    private void Start()
+    {
+        rend = playerTower.GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseDown()
+    {
+        if (playerTower != null)
+        {
+            Debug.Log("Can't Move There! - TODO: Display in Game");
+            return;
+        }
+
+        //Move Player
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
